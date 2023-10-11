f145bef359f5d2bbf65c5cbccf1a33ef18227c3e
Lynzus <danielenrriquel@gmail.com>
2023-10-11 15:31:58 -0400

    feat: added all functions

--- hack_5.py
+++ hack_5.py
@@ -4,5 +4,13 @@ text: "fooziman" output => "f00z1m@n"
 
 def fn_hack_5():
     result = "fooziman"
-    #...
-    return result  
+    x = list (result)
+    for i in range(len(x)):
+        if x[i]=="o":
+            x[i]="0"
+        elif x[i]=="i":
+            x[i]="1"
+        elif x[i]=="a":
+            x[i]="@"
+    return ''.join(x)
+print(fn_hack_5())
--- hack_9.py
+++ hack_9.py
@@ -4,5 +4,17 @@ loop: while [1,2,3] ouput => [1,'@',2,'@',3,'@']
 
 def fn_hack_9():
     result = [1,2,3]
-    #...
+    result2 = []
+    i=0
+    bandera = 1
+    while i != len(result)+1:
+        if bandera == 0:
+            result.insert(i,'@')
+            bandera = 1
+            i+=1
+        elif bandera == 1:
+            bandera = 0
+            i+=1
+        print(result)
     return result  
+fn_hack_9()
